698adf590396d2b6386ea7eb5c3e5f5375e23735
maoyu <maoyu417@gmail.com>
2019-07-04 11:37:11 +0800

    完善消费记录功能

--- pages/paymentList/paymentList.js
+++ pages/paymentList/paymentList.js
@@ -1,4 +1,7 @@
 // pages/paymentList/paymentList.js
+let request = require('../../operation/operation.js')
+let util = require('../../utils/util.js')
+
 Page({
 
   /**
@@ -12,7 +15,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getOrders()
   },
 
   /**
@@ -57,10 +60,52 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  getOrders:function() {
+    let that = this
 
+    wx.showLoading({
+      title: '请稍候',
+      mask:true
+    })
+
+    request.getRequest('/orders?category=client_orders',null,true)
+    .then(data => {
+      wx.hideLoading()
+      console.log(data)
+      that.renderOrdersList(data.items)
+    }).catch(e => {
+      wx.hideLoading()
+      wx.showToast({
+        title: e.msg,
+      })
+    })    
+  },
+
+  renderOrdersList:function(orders) {
+    for (let index = 0, size = orders.length; index < size; index++) {
+      if ( 0 == orders[index].type) {
+        orders[index].typeDesc = '预约洗车'
+      }else if ( 1 == orders[index].type) {
+        orders[index].typeDesc = '会员充值'
+      }
+
+      if ('created' == orders[index].state) {
+        orders[index].stateDesc = '已预约'
+      }else if ('canceled' == orders[index].state) {
+        if (null == orders[index].clerkUserSid) {
+          orders[index].stateDesc = '用户取消预约'
+        }else {
+          orders[index].stateDesc = '店员取消预约'
+        }
+      }else if ('finished' == orders[index].state) {
+        orders[index].stateDesc = '已完成'
+      }
+
+      orders[index].datetimeDesc = orders[index].date + ' ' + util.formatTime(orders[index].time)
+    }
+
+    this.setData({
+      orders:orders
+    })
   }
 })
